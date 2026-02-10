8213775845a6aac422867a765a4d628b0e1ecc73
Markus Cemi <markus.lindberg@cemi.se>
2026-02-10 14:19:47 +0000

    File changes

--- src/components/report/ReportContent.jsx
+++ src/components/report/ReportContent.jsx
@@ -269,11 +269,9 @@ return (
                     )}
                   </div>
                 </div>
-                <div className="page-footer">Page {index + 4}</div>
               </div>
             ))}
           </div>
-          <div className="page-footer">Page 3</div>
       </div>
     </>
   );
