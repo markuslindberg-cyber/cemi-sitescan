f09be726b780469f11c0fcdcabe8923579e65102
Markus Cemi <markus.lindberg@cemi.se>
2026-03-23 05:35:59 +0000

    File changes

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -157,6 +157,7 @@ export default function UsersPage() {
   );
 
   return (
+    <>
     <div className="p-4 md:p-8">
       <div className="max-w-6xl mx-auto">
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4 mb-8">
